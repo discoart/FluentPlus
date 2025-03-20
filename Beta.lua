2f91aede712400987f4357cd768fff02cf339249
discoart <russiancybercrime@proton.me>
2025-03-20 15:59:18 +0300

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -1,9 +1,9 @@
 --[[             |
 '                |   Last changes:     
-FluentPlus 1.2.2 |   31.01 - added Show_Assets toggle. Soon ill make normal bypass.
-dsc.gg/hydrahub  |   29.01 - well well well removed last update, added "Bloody" theme and fluent-plus settings 😉
-'                |   01.01 - fixed this file and mobile support, added a "GUI dragging cooldown".
-]]--             |
+FluentPlus 1.2.2 |   06.03 - Fixed some themes and mobile button, added normal bypass to Fisch. 
+dsc.gg/hydrahub  |   31.01 - added Show_Assets toggle. Soon ill make normal bypass.
+'                |   29.01 - well well well removed last update, added "Bloody" theme and fluent-plus settings 😉
+]]--    
 
 --- FLUENT PLUS SETTINGS ---
 local Show_Button = false -- Shows the button for toggle fluent ui manually. If "false", works only on mobile, if "true", works everytime.
@@ -2016,7 +2016,7 @@ Components.Tab = (function()
 			Name = Title,
 			Type = "Tab",
 		}
-		
+
 		if not fischbypass then 
 			if Library:GetIcon(Icon) then
 				Icon = Library:GetIcon(Icon)
@@ -2026,7 +2026,7 @@ Components.Tab = (function()
 				Icon = nil
 			end
 		end
-		
+
 		Tab.Frame = New("TextButton", {
 			Size = UDim2.new(1, 0, 0, 34),
 			BackgroundTransparency = 1,
@@ -4920,18 +4920,19 @@ ElementsTable.Input = (function()
 			Library:SafeCallback(Input.Callback, Input.Value)
 			Library:SafeCallback(Input.Changed, Input.Value)
 		end
-
-if Input.Finished then
-    AddSignal(Box.FocusLost, function(enter)
-        if enter then
-            Input:SetValue(Box.Text)
-        end
-    end)
-else
-    AddSignal(Box.FocusLost, function()
-        Input:SetValue(Box.Text)
-    end)
-end
+		
+		if Input.Finished then
+			AddSignal(Box.FocusLost, function(enter)
+				if not enter then
+					return
+				end
+				Input:SetValue(Box.Text)
+			end)
+		else
+			AddSignal(Box:GetPropertyChangedSignal("Text"), function()
+				Input:SetValue(Box.Text)
+			end)
+		end
 
 		function Input:OnChanged(Func)
 			Input.Changed = Func
@@ -5786,9 +5787,9 @@ local Icons = {
 	["lucide-dumbbell"] = "rbxassetid://18273453053"
 }
 function Library:GetIcon(Name)
-		if Name ~= nil and Icons["lucide-" .. Name] then
-			return Icons["lucide-" .. Name]
-		end
+	if Name ~= nil and Icons["lucide-" .. Name] then
+		return Icons["lucide-" .. Name]
+	end
 	return nil
 end
 
@@ -6194,9 +6195,9 @@ local InterfaceManager = {} do
 		local path = self.Folder .. "/options.json"
 		if isfile(path) then
 			local data = readfile(path)
-			
+
 			if not RunService:IsStudio() then local success, decoded = pcall(httpService.JSONDecode, httpService, data) end
-			
+
 			if success then
 				for i, v in next, decoded do
 					InterfaceManager.Settings[i] = v
@@ -6494,5 +6495,258 @@ AddSignal(MinimizeButton.MouseButton1Click, function()
 end)
 
 task.wait(0.1)
+
+local Window = Fluent:CreateWindow({
+	Title = "Fluent " .. Fluent.Version,
+	SubTitle = "by dawid",
+	TabWidth = 160,
+	Size = UDim2.fromOffset(580, 460),
+	Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+	Theme = "Dark",
+	MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+	Main = Window:AddTab({ Title = "Main", Icon = "" }),
+	Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local Options = Fluent.Options
+
+do 
+	Fluent:Notify({
+		Title = "Notification",
+		Content = "This is a notification",
+		SubContent = "SubContent", -- Optional
+		Duration = 5 -- Set to nil to make the notification not disappear
+	})
+
+
+
+	Tabs.Main:AddParagraph({
+		Title = "Paragraph",
+		Content = "This is a paragraph.\nSecond line!"
+	})
+
+
+
+	Tabs.Main:AddButton({
+		Title = "Button",
+		Description = "Very important button",
+		Callback = function()
+			Window:Dialog({
+				Title = "Title",
+				Content = "This is a dialog",
+				Buttons = {
+					{
+						Title = "Confirm",
+						Callback = function()
+							print("Confirmed the dialog.")
+						end
+					},
+					{
+						Title = "Cancel",
+						Callback = function()
+							print("Cancelled the dialog.")
+						end
+					}
+				}
+			})
+		end
+	})
+
+
+
+	local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
+
+	Toggle:OnChanged(function()
+		print("Toggle changed:", Options.MyToggle.Value)
+	end)
+
+	Options.MyToggle:SetValue(false)
+
+
+
+	local Slider = Tabs.Main:AddSlider("Slider", {
+		Title = "Slider",
+		Description = "This is a slider",
+		Default = 2,
+		Min = 0,
+		Max = 5,
+		Rounding = 1,
+		Callback = function(Value)
+			print("Slider was changed:", Value)
+		end
+	})
+
+	Slider:OnChanged(function(Value)
+		print("Slider changed:", Value)
+	end)
+
+	Slider:SetValue(3)
+
+
+
+	local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
+		Title = "Dropdown",
+		Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+		Multi = false,
+		Default = 1,
+	})
+
+	Dropdown:SetValue("four")
+
+	Dropdown:OnChanged(function(Value)
+		print("Dropdown changed:", Value)
+	end)
+
+
+
+	local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
+		Title = "Dropdown",
+		Description = "You can select multiple values.",
+		Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+		Multi = true,
+		Default = {"seven", "twelve"},
+	})
+
+	MultiDropdown:SetValue({
+		three = true,
+		five = true,
+		seven = false
+	})
+
+	MultiDropdown:OnChanged(function(Value)
+		local Values = {}
+		for Value, State in next, Value do
+			table.insert(Values, Value)
+		end
+		print("Mutlidropdown changed:", table.concat(Values, ", "))
+	end)
+
+
+
+	local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
+		Title = "Colorpicker",
+		Default = Color3.fromRGB(96, 205, 255)
+	})
+
+	Colorpicker:OnChanged(function()
+		print("Colorpicker changed:", Colorpicker.Value)
+	end)
+
+	Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
+
+
+
+	local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
+		Title = "Colorpicker",
+		Description = "but you can change the transparency.",
+		Transparency = 0,
+		Default = Color3.fromRGB(96, 205, 255)
+	})
+
+	TColorpicker:OnChanged(function()
+		print(
+			"TColorpicker changed:", TColorpicker.Value,
+			"Transparency:", TColorpicker.Transparency
+		)
+	end)
+
+
+
+	local Keybind = Tabs.Main:AddKeybind("Keybind", {
+		Title = "KeyBind",
+		Mode = "Toggle", -- Always, Toggle, Hold
+		Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
+
+		-- Occurs when the keybind is clicked, Value is `true`/`false`
+		Callback = function(Value)
+			print("Keybind clicked!", Value)
+		end,
+
+		-- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
+		ChangedCallback = function(New)
+			print("Keybind changed!", New)
+		end
+	})
+
+	-- OnClick is only fired when you press the keybind and the mode is Toggle
+	-- Otherwise, you will have to use Keybind:GetState()
+	Keybind:OnClick(function()
+		print("Keybind clicked:", Keybind:GetState())
+	end)
+
+	Keybind:OnChanged(function()
+		print("Keybind changed:", Keybind.Value)
+	end)
+
+	task.spawn(function()
+		while true do
+			wait(1)
+
+			-- example for checking if a keybind is being pressed
+			local state = Keybind:GetState()
+			if state then
+				print("Keybind is being held down")
+			end
+
+			if Fluent.Unloaded then break end
+		end
+	end)
+
+	Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
+
+
+	local Input = Tabs.Main:AddInput("Input", {
+		Title = "Input",
+		Default = "Default",
+		Placeholder = "Placeholder",
+		Numeric = false, -- Only allows numbers
+		Finished = false, -- Only calls callback when you press enter
+		Callback = function(Value)
+			print("Input changed:", Value)
+		end
+	})
+
+	Input:OnChanged(function()
+		print("Input updated:", Input.Value)
+	end)
+end
+
+
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- InterfaceManager (Allows you to have a interface managment system)
+
+-- Hand the library over to our managers
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+
+Window:SelectTab(1)
+
+Fluent:Notify({
+	Title = "Fluent",
+	Content = "The script has been loaded.",
+	Duration = 8
+})
 
 return Library, SaveManager, InterfaceManager, Mobile
